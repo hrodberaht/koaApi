64a630069cebee5c27af6af078e7a1e691abae44
hrodberaht <robpszczolka@gmail.com>
2018-08-10 20:07:33 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const Koa = require("koa");
+const Router = require("koa-router");
+
+const app = new Koa();
+const router = new Router();
+
+router.get("/", (ctx) => {
+    ctx.body = { message: "hello" };
+})
+
+app
+.use(router.routes())
+.use(router.allowedMethods());
+
+app.listen(3000,() => {
+    console.log("server starts");
+})
+
