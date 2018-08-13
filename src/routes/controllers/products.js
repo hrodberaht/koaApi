465b50e1cdd0c9a9241d8ad1159948cd8c971755
Robert <seedraev@gmail.com>
2018-08-13 18:42:16 +0200

    make get response for ember app

--- src/routes/controllers/products.js
+++ src/routes/controllers/products.js
@@ -2,7 +2,26 @@ const Product = require("../../models/product");
 
 async function findAll(ctx) {
   const products = await Product.find({});
-  ctx.body = products;
+  const data = [];
+  let id = 1;
+
+  products.map(prod => {
+    const product = {
+      type: "products",
+      id: id++,
+      attributes: {
+        name: prod.name,
+        ean: prod.ean,
+        price: prod.price
+      }
+    };
+
+    data.push(product);
+  });
+
+  ctx.body = {
+    data: data
+  };
 }
 
 module.exports = {
